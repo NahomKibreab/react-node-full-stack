b10cb1d8d998f1fdc05cae56bc389540a7ed2057
NahomKibreab <nknahom@gmail.com>
2020-05-30 22:16:18 -0600

    static frontend saved in backend src folder

---
+++ my-blog-frontend/build/precache-manifest.2504d7e063381aef0f35994325edb94e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0767774b4c6896d96d57c5d044a7b17a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c9efb3d51613e70c55",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "a1850b1e29f83404fcfa",
+    "url": "/static/js/2.11aa829e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11aa829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c9efb3d51613e70c55",
+    "url": "/static/js/main.d9fc91dc.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
